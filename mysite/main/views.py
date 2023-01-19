3a5e8c2ffc0ad47eea5610a5d85aae8fb89ef271
Godlia <52934844+Godlia@users.noreply.github.com>
2023-01-19 15:51:49 +0100

    Models

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -6,4 +6,4 @@ def index(response):
     return HttpResponse("<h1>Hello, World!</h1>")
 
 def v1(response):
-    return HttpResponse("<h1>Hello, World! v1</h1>")
+    return HttpResponse("<h1>Hello, World! v1</h1><style>body {background-color: #222222;}</style>")
